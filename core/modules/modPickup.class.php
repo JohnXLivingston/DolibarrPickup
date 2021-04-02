af0f004c7b924e240bb22baf5aa99a2c5df2b6c9
Jonathan Dollé <git@jonathandolle.fr>
2021-04-02 17:38:36 +0200

    Module number.

--- core/modules/modPickup.class.php
+++ core/modules/modPickup.class.php
@@ -45,7 +45,7 @@ class modPickup extends DolibarrModules
 
         // Id for module (must be unique).
         // Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-        $this->numero = 500000; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
+        $this->numero = 469001;
         // Key text used to identify module (for permissions, menus, etc...)
         $this->rights_class = 'pickup';
         // Family can be 'base' (core modules),'crm','financial','hr','projects','products','ecm','technic' (transverse modules),'interface' (link with external tools),'other','...'
