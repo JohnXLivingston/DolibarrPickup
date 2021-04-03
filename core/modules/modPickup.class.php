139efc31d7e90df2363ed0ab985d56e854e7fdbb
Jonathan Dollé <git@jonathandolle.fr>
2021-04-03 16:29:33 +0200

    Fix typo.

--- core/modules/modPickup.class.php
+++ core/modules/modPickup.class.php
@@ -345,7 +345,7 @@ class modPickup extends DolibarrModules
                                 'langs'=>'pickup@pickup',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
                                 'position'=>1100+$r,
                                 'enabled'=>'$conf->pickup->enabled',  // Define condition to show or hide menu entry. Use '$conf->pickup->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-                                'perms'=>'$users->rights->pickup->configure',               // Use 'perms'=>'$user->rights->pickup->level1->level2' if you want your menu with a permission rules
+                                'perms'=>'$user->rights->pickup->configure',               // Use 'perms'=>'$user->rights->pickup->level1->level2' if you want your menu with a permission rules
                                 'target'=>'',
                                 'user'=>0);				                // 0=Menu for internal users, 1=external users, 2=both
         
