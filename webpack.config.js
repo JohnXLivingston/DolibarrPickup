8769dcf7bed2f0923ceaf6da107923c3e5023054
John Livingston <git@john-livingston.fr>
2021-04-05 19:05:42 +0200

    WIP: retrieving code from Dolinput. NPM and typescript env.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/mobile.ts',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'mobile.js',
+    path: path.resolve(__dirname, 'js', 'content'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+}
