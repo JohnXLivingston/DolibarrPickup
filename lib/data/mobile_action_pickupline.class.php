63898d2de032857f7541da34f70de620e9e86fb8
John Livingston <git@john-livingston.fr>
2021-04-27 11:53:22 +0200

    Mobile: save pickupline.

--- lib/data/mobile_action_pickupline.class.php
+++ lib/data/mobile_action_pickupline.class.php
@@ -2,13 +2,28 @@
 dol_include_once('/pickup/lib/data/mobile_action.class.php');
 
 class DataMobileActionPickupline extends DataMobileAction {
-  // TODO
-  // public function action_save() {
-  //   dol_syslog(__METHOD__, LOG_DEBUG);
-  //   global $user;
+  public function action_save() {
+    dol_syslog(__METHOD__, LOG_DEBUG);
+    global $user;
+    $db = $this->db;
 
-  //   dol_include_once('/pickup/class/pickup.class.php');
-  //   dol_include_once('/pickup/class/pickupline.class.php');
+    dol_include_once('/pickup/class/pickup.class.php');
+    dol_include_once('/pickup/class/pickupline.class.php');
     
-  // }
+    $pickup = new Pickup($db);
+    if ($pickup->fetch(GETPOST('pickup', 'int')) <= 0) {
+      return 0;
+    }
+
+    $product_id = GETPOST('product', 'int');
+    $qty = GETPOST('qty', 'int');
+    $pickupline = $pickup->initPickupLine($product_id, $qty);
+    $id = $pickupline->create($user);
+    if (!$id || $id <= 0) {
+      $this->_log_object_errors(__METHOD__, $pickupline);
+      return 0;
+    }
+
+    return array("rowid" => $id);
+  }
 }
