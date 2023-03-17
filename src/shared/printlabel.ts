18e121025b945842deccb544bff4624394e216a0
John Livingston <git@john-livingston.fr>
2023-03-17 14:34:46 +0100

    Impression étiquette: loading.

--- src/shared/printlabel.ts
+++ src/shared/printlabel.ts
@@ -5,13 +5,27 @@ function setPrintLabelUrl (url: string): void {
 }
 
 function _print (dom: JQuery | HTMLElement, url: string): void {
+  const domElement: HTMLElement | undefined = ('get' in dom) ? dom.get(0) : dom
+  if (!domElement) { return }
+
   const iframe: HTMLIFrameElement = document.createElement('iframe')
   iframe.setAttribute('src', url)
   iframe.style.display = 'none'
-  dom.after(iframe)
+
+  let waitingElement: HTMLElement | undefined
+  if (domElement.classList.contains('btn')) {
+    // It seems we are using a Boostrap button (mobile app), so we can use Bootstrap spinner element
+    waitingElement = document.createElement('span')
+    waitingElement.classList.add('spinner-border')
+    waitingElement.classList.add('spinner-border-sm')
+    dom.prepend(waitingElement)
+  }
+
+  domElement.after(iframe)
 
   iframe.onload = () => {
     iframe.contentWindow?.print()
+    waitingElement?.remove()
   }
 }
 
