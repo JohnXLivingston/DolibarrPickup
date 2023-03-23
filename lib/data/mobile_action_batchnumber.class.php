b16257f610fa2d853012b42f3d101b2a39a1178d
John Livingston <git@john-livingston.fr>
2023-03-23 17:41:43 +0100

    Bump version 2.0.0-beta2

--- lib/data/mobile_action_batchnumber.class.php
+++ lib/data/mobile_action_batchnumber.class.php
@@ -6,7 +6,7 @@ dol_include_once('/pickup/lib/data/mobile_action.class.php');
 class DataMobileActionBatchNumber extends DataMobileAction {
   public function action_generate() {
     global $conf;
-    
+
     dol_syslog(__METHOD__, LOG_DEBUG);
     dol_include_once('/pickup/class/pbatch.class.php');
 
